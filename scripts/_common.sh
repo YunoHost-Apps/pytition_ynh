fd5207095e1d89f4288aebf666112748f822fde7
Félix Piédallu <felix@piedallu.me>
2022-04-11 17:16:03 +0200

    Handle change_url

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -23,3 +23,13 @@ pkg_dependencies=(
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
+
+
+generate_secret_key() {
+    (
+        set +o nounset
+        source "${final_path}/venv/bin/activate"
+        set -o nounset
+        python3 -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())"
+    )
+}
