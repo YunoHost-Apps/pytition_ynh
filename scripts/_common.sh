5c69e2720335e0e46c5e40c4cc14e3038cfb1451
yalh76 <yalh@yahoo.com>
2022-07-29 23:30:24 +0200

    Apply example_ynh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,25 +5,11 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies=(
-    python3 virtualenv
-    gettext libzip-dev libssl-dev
-    mariadb-server default-libmysqlclient-dev
-    uwsgi uwsgi-plugin-python3 python3-uwsgidecorators
-)
+pkg_dependencies="python3 virtualenv gettext libzip-dev libssl-dev mariadb-server default-libmysqlclient-dev uwsgi uwsgi-plugin-python3 python3-uwsgidecorators"
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
-
-#=================================================
-# EXPERIMENTAL HELPERS
-#=================================================
-
-#=================================================
-# FUTURE OFFICIAL HELPERS
-#=================================================
-
 
 generate_secret_key() {
     (
@@ -33,3 +19,11 @@ generate_secret_key() {
         python3 -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())"
     )
 }
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
